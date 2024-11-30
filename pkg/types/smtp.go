18b2657677646d85f5d5e24004469390096fce2d
Ricardo Alcantara <alcantarafox@yahoo.com.br>
2024-11-30 03:26:11 +0000

    closes #10

--- pkg/types/smtp.go
+++ pkg/types/smtp.go
@@ -6,19 +6,19 @@ type CreateSmtpDto struct {
 	Server   string `json:"server" binding:"required"`
 	Port     uint16 `json:"port" binding:"required"`
 	Email    string `json:"email" binding:"required"`
-	User     string `json:"user" binding:"required"`
-	Password string `json:"password" binding:"required"`
+	User     string `json:"user"`
+	Password string `json:"password"`
 	Default  bool   `json:"default"`
 }
 
 type UpdateSmtpDto struct {
-	Name     *string `json:"name" binding:"required"`
-	Slug     *string `json:"slug" binding:"required"`
-	Server   *string `json:"server" binding:"required"`
-	Port     *uint16 `json:"port" binding:"required"`
-	Email    *string `json:"email" binding:"required"`
-	User     *string `json:"user" binding:"required"`
-	Password *string `json:"password" binding:"required"`
+	Name     *string `json:"name"`
+	Slug     *string `json:"slug"`
+	Server   *string `json:"server"`
+	Port     *uint16 `json:"port"`
+	Email    *string `json:"email"`
+	User     *string `json:"user"`
+	Password *string `json:"password"`
 	Default  *bool   `json:"default"`
 }
 
